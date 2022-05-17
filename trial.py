df6f4f7865afcdab1cfc6fe3e67211527602d94c
Miaojing Yang <91395684+Yvonne0904@users.noreply.github.com>
2022-05-16 23:25:09 -0400

    Update trial.py

--- trial.py
+++ trial.py
@@ -28,11 +28,11 @@ df_cbg = spark.createDataFrame(df_cbg)
 
 #Filter the palce
 placeList = df_supermarket.select("safegraph_placekey").rdd.flatMap(lambda x: x).collect()
-df_pattern = df_pattern.filter(F.col("placekey").isin(placeList))
+df_pattern = df_pattern.filter(F.col("placekey").isin(placeList)).cache()
 
-df_pattern1 = df_pattern.select("placekey","poi_cbg","visitor_home_cbgs", date_format("date_range_start", "yyyy-MM").alias("Formatted_start_date"),date_format("date_range_end", "yyyy-MM").alias("Formatted_end_date"))
+df_pattern = df_pattern.select("placekey","poi_cbg","visitor_home_cbgs", date_format("date_range_start", "yyyy-MM").alias("Formatted_start_date"),date_format("date_range_end", "yyyy-MM").alias("Formatted_end_date"))
 dateList = ['2019-03','2019-10','2020-03','2020-10']
-df_pattern2 = df_pattern1.filter(F.col("Formatted_start_date").isin(dateList) | F.col("Formatted_end_date").isin(dateList))
+df_pattern = df_pattern.filter(F.col("Formatted_start_date").isin(dateList) | F.col("Formatted_end_date").isin(dateList))
 
 def extract_date(col1,col2):
   res = ''
@@ -45,7 +45,7 @@ def extract_date(col1,col2):
   return res
 
 dateUdf = F.udf(extract_date,T.StringType())
-df_pattern_date = df_pattern2.withColumn('date',dateUdf('Formatted_start_date','Formatted_end_date'))
+df_pattern = df_pattern.withColumn('date',dateUdf('Formatted_start_date','Formatted_end_date'))
 
 def f_key(col1):
   visitor_home_cbg = json.loads(col1)
@@ -59,17 +59,17 @@ def f_value(col1):
   value_res = []
   for i in visitor_home_cbg.values():
     value_res.append(i)
-  return 
+  return value_res
   
 keyUdf = F.udf(f_key,T.ArrayType(T.StringType()))
 valueUdf = F.udf(f_value,T.ArrayType(T.IntegerType()))
-df_pattern3 = df_pattern_date.withColumn('res_key',keyUdf("visitor_home_cbgs")).withColumn('res_value',valueUdf("visitor_home_cbgs"))
+df_pattern = df_pattern.withColumn('res_key',keyUdf("visitor_home_cbgs")).withColumn('res_value',valueUdf("visitor_home_cbgs"))
 
-df_pattern5 = df_pattern3.withColumn("new", F.arrays_zip("res_key", "res_value"))\
+df_pattern = df_pattern.withColumn("new", F.arrays_zip("res_key", "res_value"))\
        .withColumn("new", F.explode("new"))\
        .select('date',"poi_cbg", F.col("new.res_key").alias("home_key"), F.col("new.res_value").alias("people_number"))
 
-df_pattern5 = df_pattern5.filter(F.col('home_key').substr(1, 2) == '36')
+df_pattern5 = df_pattern.filter(F.col('home_key').substr(1, 2) == '36')
 
 join_df1 = df_pattern5.join(df_cbg,F.col('poi_cbg')==F.col('cbg_fips'),'inner')\
 .select(['date','poi_cbg','home_key','people_number','latitude','longitude']).withColumnRenamed('latitude','poi_lat').withColumnRenamed('longitude','poi_long')
@@ -102,9 +102,6 @@ res_df = distance_res.select('date','poi_cbg','people_number','weighted_distance
 
 grouped_df = res_df.groupby('date','poi_cbg').agg(
     F.sum('people_number'),
-    
     (F.sum('weighted_distance')/F.sum('people_number')).alias('weighted_distance')
 )
 df_pivot_res = grouped_df.groupBy("poi_cbg").pivot('date').sum('weighted_distance').sort('poi_cbg').rdd.saveAsTextFile(sys.argv[1])
-
-
